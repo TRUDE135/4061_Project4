92d9ab9212a772e361093141021070a370a54ef4
TRUDE135 <72237941+TRUDE135@users.noreply.github.com>
2020-12-15 17:06:40 -0600

    Update util.c
    commit msg

--- p4/p4/util.c
+++ p4/p4/util.c
@@ -15,6 +15,30 @@
 #include <unistd.h>
 #include "util.h"
 
+#define BACKLOG 20
+#define REQSIZE 2048  //size of GET request, 2048th byte is NULL
+#define PATHSIZE 1024 //size of file path in get_request
+
+int socket_fd, client_fd;
+struct sockaddr_in server_addr; /* my address */
+struct sockaddr_in client_addr; /* client's address */
+unsigned int addr_size;
+int enable = 1;
+
+//  find an occurence of .. or // in the path
+int isLegalPath(char *path){
+  int i;
+  int s = strlen(path);
+
+  for(i=1; i<s; i++){
+    if(path[i] == '/' && path[i-1] == '/')
+      return 0;
+    if(path[i] == '.' && path[i-1] == '.')
+      return 0;
+  }
+  return 1;
+}
+
 /**********************************************
  * init
    - port is the number of the port you want the server to be
@@ -26,6 +50,32 @@
    - if init encounters any errors, it will call exit().
 ************************************************/
 void init(int port) {
+  //  Create socket
+  socket_fd = socket(PF_INET, SOCK_STREAM, 0);
+  if(socket_fd == -1){
+    perror("In init(): Error creating socket.");
+    exit(1);
+  }
+  //  Bind the socket to a network address
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  //  Set the reuse option
+  if( setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, (char*)&enable, sizeof(int)) == -1) {
+    perror("In init(): Cannot set socket reuse option.");
+    exit(1);
+  }
+  // Bind the socket command
+  if ( bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    perror("In init(): Unable to bind.");
+    exit(1);
+  }
+  // Setup a queue to listen to oncoming requests (backlog upto 20 requests in queue)
+  if (  listen(socket_fd, BACKLOG) == -1) {
+    perror("In init(): Unable to listen.");
+    exit(1);
+  }
+  return;
 }
 
 /**********************************************
@@ -36,6 +86,14 @@ void init(int port) {
    - if the return value is negative, the request should be ignored.
 ***********************************************/
 int accept_connection(void) {
+  int connection_fd;
+  addr_size = sizeof(client_addr);
+  connection_fd = accept(socket_fd, (struct sockaddr *)&client_addr, &addr_size);
+  if(connection_fd == -1){
+    perror("In accept_connection(): Unable to accept connection.");
+    return -1;
+  }
+  return connection_fd;
 }
 
 /**********************************************
@@ -54,13 +112,51 @@ int accept_connection(void) {
      specific 'connection'.
 ************************************************/
 int get_request(int fd, char *filename) {
+
+  char reqMsg[REQSIZE];
+  char reqName[10];       // aka GET
+  char reqPath[PATHSIZE]; // aka file path
+  char reqType[10];       // aka HTTP/1.1
+  int readSize = 0;
+  readSize = read(fd, reqMsg, REQSIZE-1);
+
+  if(readSize < 0){
+    perror("In get_request(): Request read problem.");
+    return -1;  //if read() returns negative, a read error must have occured
+  }
+  //  This should separate the important info from the request into three char buffers
+  //  These three, in order, should be the first line of the request if printed in its entirety
+  sscanf(reqMsg, "%s %s %s", reqName, reqPath, reqType);
+
+  if(strcmp(reqName,"GET"))   // Not a get request
+    return -1;
+  if(strlen(reqPath) <= 0 || !isLegalPath(reqPath))    // Empty file path case or faulty read from sscanf()
+    return -1;                                          //  Or path contains .. or //
+
+  //  If reach this point, the request contains the two strings as specified
+
+  // Debug print statements
+  // printf("reqMsg: %s \n", reqMsg);
+  // printf("reqName: %s \n", reqName);
+
+  // printf("reqPath: %s \n", reqPath);     --> Most important printf here(?)
+
+  // printf("reqType: %s \n", reqType);
+
+  reqMsg[readSize] = '\0';  //Null terminate end of message
+  reqPath[PATHSIZE-1] = '\0'; // Null terminate file path
+ // fprintf(stderr, "First line of request: %s \n", reqMsg);
+ // fprintf(stderr, "%s \n", reqMsg);
+  strcpy(filename, reqPath);
+
+  return 0;
 }
 
 /**********************************************
  * return_result
    - returns the contents of a file to the requesting client
    - parameters:
-      - fd is the file descriptor obtained by accept_connection()
+      - fd is the file descriptor obtained by accept_connection()     --> Socket descriptor
         to where you wish to return the result of a request
       - content_type is a pointer to a string that indicates the
         type of content being returned. possible types include
@@ -76,6 +172,21 @@ int get_request(int fd, char *filename) {
    - returns 0 on success, nonzero on failure.
 ************************************************/
 int return_result(int fd, char *content_type, char *buf, int numbytes) {
+  char header[REQSIZE];
+
+  int size = sprintf(header, 
+      "HTTP/1.1 200 OK\nContent-Type: %s\nContent-Length: %d\nConnection: Close\n\n",
+      content_type, numbytes);
+
+  write(fd, header, size);
+  write(fd, buf, numbytes);
+  
+  if(close(fd) < 0){
+    printf("\nSocket didn't close right\n");
+    return -1;
+  }
+
+  return 0;
 }
 
 /**********************************************
@@ -88,4 +199,19 @@ int return_result(int fd, char *content_type, char *buf, int numbytes) {
    - returns 0 on success, nonzero on failure.
 ************************************************/
 int return_error(int fd, char *buf) {
+  char header[REQSIZE];
+
+  int size = sprintf(header, 
+      "HTTP/1.1 404 Not Found\nContent-Type: text/html\nContent-Length: %ld\nConnection: Close\n\n",
+      strlen(buf));
+      
+  write(fd, header, size);
+  write(fd, buf, sizeof(buf));
+
+  if(close(fd) < 0){
+    printf("\nSocket didn't close right\n");
+    return -1;
+  }
+
+  return 0;
 }
